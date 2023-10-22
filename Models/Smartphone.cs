915b92b89c675a3681989ed15b870f5d4243443a
Rodrigo Silva <112216918+rodericussilva@users.noreply.github.com>
2023-10-22 15:51:03 -0300

    update

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
@@ -5,11 +10,21 @@ namespace DesafioPOO.Models
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero)
+        public string Modelo { get; set; }
+
+        private string IMEI { get; set; }
+
+        private int Memoria { get; set; }
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
+
 
         public void Ligar()
         {
--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
-﻿using DesafioPOO.Models;
+﻿using System.Globalization;
+//using System.Xml.Xsl.Runtime;
+using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "9999-9999", modelo: "STG-02", imei: "2222222", memoria: 123);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "9999-8888", modelo: "IPH-01", imei: "33333333", memoria: 999);
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram");
